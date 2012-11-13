7f78dd52a533d20c9e8cece4f55c37abd3aa3c14
shysean <shihongyang0218@gmail.com>
2012-11-13 14:32:21 +0800

    init

---
+++ MyCode/src/shihongyang/code/HelloWorld.java
@@ -0,0 +1,13 @@
+package shihongyang.code;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world.");
+	}
+
+}
